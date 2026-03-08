6722883b880c44d7e27dc7db05655fc900d0e404
Will Doan <pdoanthe96@gmail.com>
2026-03-07 23:35:32 -0500

    Added PageLoader

--- frontend/components/PageLoader.tsx
+++ frontend/components/PageLoader.tsx
@@ -1,10 +1,9 @@
 const PageLoader = () => {
   return (
-    <div>
-
+    <div className="min-h-screen bg-gray-950 flex items-center justify-center">
+      <div className="w-6 h-6 border-2 border-gray-700 border-t-emerald-400 rounded-full animate-spin" />
     </div>
   )
 }
 
 export default PageLoader
-
