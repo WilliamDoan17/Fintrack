ed01a706fdf3f9ca8ef13137c6671126cad87005
WilliamDoan17 <pdoanthe96@gmail.com>
2026-06-23 22:32:38 -0400

    feat(types/transactions): remove type from ts types for transactions

--- frontend/backend/types/transactions.ts
+++ frontend/backend/types/transactions.ts
@@ -1,10 +1,7 @@
-export type TransactionType = 'add' | 'withdraw';
-
 export interface Transaction {
   id: string,
   user_id: string,
   budget_id: string,
-  type: TransactionType,
   amount: number,
   name: string,
   created_at: string,
